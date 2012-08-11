2a6b8c29ce3cde9208e2f145c1cc3288d6e4125b
Ian Price <ianprice90@googlemail.com>
2012-08-11 15:41:23 +0100

    Add pkg-list.scm

---
+++ pkg-list.scm
@@ -0,0 +1,7 @@
+(package (fectors (0 1))
+  (synopsis "Functional Vectors in Scheme")
+  (description
+   "Functional Vectors in Scheme. Semi persistent, to allow fast
+updates to most recent version.  Not thread safe.")
+  (homepage "http://github.com/ijp/fectors")
+  (libraries "fectors.sls"))
